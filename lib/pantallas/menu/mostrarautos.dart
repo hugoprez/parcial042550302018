82b6016e987fa19a222cb9b992f1de176e69ab67
hugo antillon <hugoantillonprez@gmail.com>
2023-05-20 21:54:20 -0600

    2005202302

--- lib/pantallas/menu/mostrarautos.dart
+++ lib/pantallas/menu/mostrarautos.dart
@@ -104,7 +104,7 @@ class _MostrarVehiculosState extends State<MostrarVehiculos> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
-            await Navigator.pushNamed(context, '/add');
+            await Navigator.pushNamed(context, '/addvehiculo');
             //actualiza los datos al agregar un usuario
             setState(() {});
             //print("Actualizar");
